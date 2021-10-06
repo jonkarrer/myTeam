df14e28e9cb23b513b087ca65dd23bd9e9d398ad
jonkarrer <karrerjon@gmail.com>
2021-10-06 15:16:26 -0500

    page: about, splash & directors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const x = document.getElementById("close_nav");
 const hamburger = document.querySelector("header.mobile .hamburger");
+const nav = document.querySelector("header.mobile nav");
+
+window.onload = () => (nav.style.transition = "transform 0.6s ease");
 
 hamburger.addEventListener("click", () => toggleNav(true));
 x.addEventListener("click", () => toggleNav(false));
 
 const toggleNav = (bool) => {
-  const nav = document.querySelector("header.mobile nav");
   if (bool) return (nav.style.transform = "translateX(0)");
-  if (!bool) return (nav.style.transform = "translateX(100%)");
+  if (!bool) {
+    nav.style.transform = "translateX(-10%)";
+    setTimeout(() => (nav.style.transform = "translateX(100%)"), 300);
+  }
 };
